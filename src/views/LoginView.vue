7dde984f9cb916d79a0c7721ef973519d40a08ea
romainrhd <romain.richard.19@gmail.com>
2022-03-11 17:27:14 +0100

    Design login form

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,8 +1,18 @@
-<!-- TODO : Ajouter la notion de layout -->
+<script setup>
+import { ref } from "vue";
+const active = ref(false);
+function changeInput() {
+  active.value = !active.value;
+}
+</script>
+
 <template>
   <div class="flex flex-col m-auto bg-red-600 border border-black rounded">
     <div class="flex m-auto space-x-1">
-      <div class="bg-blue-500 p-4 m-2 rounded-3xl border-2"></div>
+      <div
+        class="p-4 m-2 rounded-3xl border-2"
+        :class="active ? 'bg-blue-600' : 'bg-blue-500'"
+      ></div>
       <div class="flex space-x-1 pt-0.5 pl-2 mt-2">
         <div>
           <div class="bg-red-600 p-1 rounded-3xl border border-black"></div>
@@ -16,12 +26,31 @@
       </div>
       <div class="px-16"></div>
     </div>
-    <div class="flex flex-row">
-      <div
-        class="w-full border-t border-black h-10 bg-blue-500"
-        id="trait"
-      ></div>
+    <div class="border-b border-black"></div>
+    <div class="flex flex-col h-80">
+      <div class="flex flex-col p-3">
+        <label for="login">Login</label>
+        <input
+          id="login"
+          class="rounded p-2"
+          type="text"
+          name="login"
+          @keypress="changeInput"
+        />
+      </div>
+      <div class="flex flex-col p-3">
+        <label for="password">Mot de passe</label>
+        <input
+          id="password"
+          class="rounded p-2"
+          type="password"
+          name="password"
+          @keypress="changeInput"
+        />
+      </div>
+      <div class="flex flex-col p-3">
+        <button class="rounded bg-green-500 py-4">Se connecter</button>
+      </div>
     </div>
-    <div class="h-80"></div>
   </div>
 </template>
